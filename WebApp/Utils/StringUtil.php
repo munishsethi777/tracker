31e076add7e11b41a8c07ba61c53bb0153d29b3c
bgaheer <baljeetgaheer@gmail.com>
2014-12-15 22:48:53 +0530

    stringutil

---
+++ WebApp/Utils/StringUtil.php
@@ -0,0 +1,11 @@
+<?php
+  class StringUtil{
+      public static function checkVal($val){
+           if($val == ""){
+               return Null;
+           }else{
+               return $val;
+           }
+      }
+  }
+?>
