eacf94ce30c8003e432185d25023b5254c2e8351
munishsethi777 <munishsethi777@gmail.com>
2014-12-18 22:31:07 +0530

    mycontacts api

--- WebApp/api/MyContacts.php
+++ WebApp/api/MyContacts.php
@@ -2,25 +2,33 @@
   require_once('../IConstants.inc');
   require_once($ConstantsArray['dbServerUrl'] ."BusinessObjects/User.php");
   require_once($ConstantsArray['dbServerUrl']. "DataStores/UserDataStore.php");
+  include($ConstantsArray['dbServerUrl'] .'scripts/log4php/Logger.php');
+  Logger::configure($ConstantsArray['dbServerUrl'] .'scripts/log4php/log4php.xml');
+  $log = Logger::getLogger('main');
+  $log->info("Making mycontacts call");
   $response = new ArrayObject();
   $response["success"] = 1;
   $response["message"]  = "";
   $response["numbers"] = 0;
   try{
     $mobileNumbers = $_GET["numbers"];
+    $log->debug("MyContacts API called numbers: ". $mobileNumbers);
+
     $userDataStore = UserDataStore::getInstance();
     $existingMobiles = $userDataStore->getExistingMobile($mobileNumbers);
     $response["numbers"] = $existingMobiles;
   }catch(Exception $e){
      $response["success"] = 0;
-     $response["message"]  = $e->getMessage(); 
+     $response["message"]  = $e->getMessage();
+     $log->error("Error occured:");
+
   }
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Credentials: true");
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Max-Age: 1000');
     header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
-    header("Content-type: application/json"); 
+    header("Content-type: application/json");
     echo json_encode($response);
     return;
 ?>
